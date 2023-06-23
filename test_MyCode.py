87a944f7c4314e55dbd746f877ab517e06e1f0da
HajarAbuRmeileh <hajar.maint@gmail.com>
2023-06-23 18:00:21 +0300

    auto image tags

--- test_MyCode.py
+++ test_MyCode.py
@@ -6,6 +6,7 @@ def test_add_numbers():
     assert MyCode.add_numbers(2, 3) == 5
     assert MyCode.add_numbers(0, 0) == 0
     assert MyCode.add_numbers(-5, 5) == 0
+    assert MyCode.add_numbers(-5, -5) == -10
 
 def test_get_user_input(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda _: '2')
